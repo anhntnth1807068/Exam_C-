96702496b2f1cca51448974c57af6e4d9abc2cea
Anh Hoc Dot <anh@hocdot.com>
2019-07-22 15:37:57 +0700

    Push hello

---
+++ exampaper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using exampaper.entity;
+
+namespace exampaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example();
+        }
+
+        public static void Example()
+        {
+            var people = new People
+            {
+                Name = "Mary",
+                Gender = true,
+                Age = 25,
+            };
+
+            Console.WriteLine("Simple properties demo");
+            Console.WriteLine("Person detail" + people.ToString());
+            people.Age = 35;
+            Console.WriteLine("Person detail(apter incrementing age):" + people.ToString());
+        }
+
+        public static void Example2()
+        {
+            Console.WriteLine("Enter a String");
+            var str = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            var word = Console.ReadLine();
+
+            var count = Regex.Matches(str, word).Count;
+            Console.WriteLine($"Word found {count} times in the string");
+        }
+    }
+}
---
+++ exampaper/entity/People.cs
@@ -0,0 +1,15 @@
+namespace exampaper.entity
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            var stringGender = Gender ? "Male" : "Female";
+            return $"Name = {Name},Gender ={stringGender},Age = {Age}";
+        }
+    }
+}
